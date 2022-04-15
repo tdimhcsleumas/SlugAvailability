63e01d72848dc53e35e52c8e084e0c0b9cc377ca
Samuel Schmidt <sadschmi@ucsc.edu>
2022-04-14 19:42:39 -0700

    update dependencies. remove App.tsx and related code

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+} from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+
   </React.StrictMode>,
   document.getElementById('root')
 );
